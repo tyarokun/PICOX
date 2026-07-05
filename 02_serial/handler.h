e06ebecb8d47b64eb1a47c01ea47a2e6c3ab5317
tyarokun <131958184+tyarokun@users.noreply.github.com>
2026-07-05 18:48:11 +0900

    Add files via upload

---
+++ 02_serial/handler.h
@@ -0,0 +1,8 @@
+#ifndef HANDLER_H
+#define HANDLER_H
+
+extern int main(void);
+extern void Reset_Handler(void);
+extern void Default_Handler(void);
+
+#endif
---
+++ 02_serial/serial.h
@@ -0,0 +1,10 @@
+#ifndef SERIAL_H
+#define SERIAL_H
+
+void serial_init(void);
+void serial_putc(char c);
+void serial_puts(const char *s);
+char serial_getc(void);
+int serial_getc_nonblock(void);
+
+#endif
